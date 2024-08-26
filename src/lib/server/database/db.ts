1c1a3737ecb0dcc02637e125184e4feef74ba1d0
ocluf <fulco.taen@gmail.com>
2024-08-26 14:53:15 +0200

    x

--- src/lib/server/database/db.ts
+++ src/lib/server/database/db.ts
@@ -1,17 +1,7 @@
 import { drizzle } from 'drizzle-orm/libsql';
-import { dev } from '$app/environment';
 import { createClient } from '@libsql/client';
 import { env } from '$env/dynamic/private';
-import * as schema from '$lib/server/database/schema'
+import * as schema from '$lib/server/database/schema';
 
-const url = dev ? 'file:local.db' : env.TURSO_DB_URL;
-
-if (!url) {
-	throw new Error('TURSO_DB_URL is not set');
-}
-if (!dev && !env.TURSO_DB_AUTH_TOKEN) {
-	throw new Error('TURSO_DB_AUTH_TOKEN is not set');
-}
-
-const libsql = createClient({ url, authToken: env.TURSO_DB_AUTH_TOKEN });
+const libsql = createClient({ url: env.TURSO_DB_URL });
 export const db = drizzle(libsql, { schema });
