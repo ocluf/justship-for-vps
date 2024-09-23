572cae7dc84f48d392e621826e8179d37a1b8eeb
ocluf <fulco.taen@gmail.com>
2024-09-23 12:46:13 +0200

    x

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,11 +8,11 @@ module.exports = {
 	node_args: '--env-file=.env',
 	deploy: {
 		production: {
-			user: 'root',
-			host: '195.201.128.2',
-			ref: 'origin/main',
-			repo: 'https://github.com/ocluf/justship-for-vps.git',
-			path: '/root/projects/test',
+			user: 'TODO_FILL_IN', // e.g root
+			host: 'TODO_FILL_IN', // e.g 195.201.128.5
+			ref: 'TODO_FILL_IN', // e.g origin/main
+			repo: 'TODO_FILL_IN', // e.g https://github.com/ocluf/justship-for-vps.git
+			path: 'TODO_FILL_IN', // e.g /root/projects/justship-for-vps
 			'post-deploy':
 				'pnpm install && pnpm run build && pm2 reload ecosystem.config.cjs --env production'
 		}
